c0d19c648bf9b16653df028e7bd15490d6346484
dutsler1 <dutsler1@gmail.com>
2026-01-21 01:42:20 -0700

    added the user option to select the difficulty they wish

---
+++ number_guessing_game/main.py
@@ -0,0 +1,6 @@
+import random_generator
+
+print("Select a difficulty to continue: ")
+user_difficulty = random_generator.get_random_number(input("easy, medium, or hard: "))
+print(f"You selected {user_difficulty} difficulty.")
+
