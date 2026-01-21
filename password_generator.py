a79566591fa954c9a8d7d00e80d12e3cb6672cae
dutsler1 <dutsler1@gmail.com>
2026-01-20 23:19:05 -0700

    created a new file that holds characters and moved the character sets from password_generator.py to chars.py

--- password_generator.py
+++ password_generator.py
@@ -1,14 +1,8 @@
 import random
-LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
-UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-DIGITS = '0123456789'
-SPECIAL_CHARACTERS = '!@#$%^&*()-_=+[]{}|;:,.<>?/'
-password_string = LOWERCASE_LETTERS + UPPERCASE_LETTERS + DIGITS + SPECIAL_CHARACTERS
+import chars
 
 def random_password(min_length=10,max_length=16):
     #generates a random password
-    password_string = LOWERCASE_LETTERS + UPPERCASE_LETTERS + DIGITS + SPECIAL_CHARACTERS
-    password = ''
     length = random.randint(min_length, max_length)
     for i in range(length):
         password += random.choice(password_string)
