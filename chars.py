5fc0f5adf245d21a47dbc81a8a961389325d8175
dutsler1 <dutsler1@gmail.com>
2026-01-21 01:26:11 -0700

    Remove duplicate files from root directory

--- chars.py
+++
@@ -1,9 +0,0 @@
-
-# character sets for the password generator
-min_length = 10
-max_length = 16
-LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
-UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-DIGITS = '0123456789'
-SPECIAL_CHARACTERS = '!@#$%^&*()-_=+[]{}|;:,.<>?/'
-password_string = LOWERCASE_LETTERS + UPPERCASE_LETTERS + DIGITS + SPECIAL_CHARACTERS
