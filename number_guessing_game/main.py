c552ec0637afcf881ede8f5eade62d8491c4b6be
dutsler1 <dutsler1@gmail.com>
2026-01-21 02:19:38 -0700

    added the user attempts to medium and hard

--- number_guessing_game/main.py
+++ number_guessing_game/main.py
@@ -27,7 +27,28 @@ elif user_difficulty.lower() == 'medium':
     random_number = random_generator.get_random_number('medium')
     print('Choose a number between 1 and 300')
     user_number = int(input())
+    while user_number != random_number:
+        attempts += 1
+        if user_number < random_number:
+            print('your guess is to low, try again')
+            user_number = int(input())
+        elif user_number > random_number:
+            print('your guess is to high, try again')
+            user_number = int(input())
+    print('you guessed the correct number, you win!')
+    print(f'It took you {attempts} attempts to guess the correct number.')
+
 elif user_difficulty.lower() == 'hard':
     random_number = random_generator.get_random_number('hard')
     print('Choose a number between 1 and 1000')
     user_number = int(input())
+    while user_number != random_number:
+        attempts += 1
+        if user_number < random_number:
+            print('your guess is to low, try again')
+            user_number = int(input())
+        elif user_number > random_number:
+            print('your guess is to high, try again')
+            user_number = int(input())
+    print('you guessed the correct number, you win!')
+    print(f'It took you {attempts} attempts to guess the correct number.')
