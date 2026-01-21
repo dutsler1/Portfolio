881667916af5dd168713dcd1e7a622f700a25496
dutsler1 <dutsler1@gmail.com>
2026-01-21 01:34:41 -0700

    added difficulty levels and a function for those levels to generate a random number

--- number_guessing_game/random_generator.py
+++ number_guessing_game/random_generator.py
@@ -2,3 +2,13 @@ import random
 random_number_easy = random.randint(1, 100)
 random_number_medium = random.randint(1, 300)
 random_number_hard = random.randint(1, 1000)
+
+def get_random_number(difficulty):
+    if difficulty.lower() == 'easy':
+        return random_number_easy
+    elif difficulty.lower() == 'medium':
+        return random_number_medium
+    elif difficulty.lower() == 'hard':
+        return random_number_hard
+    else:
+        print("invalid difficulty level")
