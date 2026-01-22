c253951d34034c4c713b2a6a670f6c8891ed5f7e
dutsler1 <dutsler1@gmail.com>
2026-01-22 00:32:51 -0700

    added user input functions to main.py

--- basic_calculator/functions.py
+++ basic_calculator/functions.py
@@ -1,12 +1,18 @@
 def add(num1, num2):
-    return num1 + num2
+    answer = num1 + num2
+    return answer
 def subtract(num1, num2):
-    return num1 - num2
+    answer = num1 - num2
+    return answer
 def multiply(num1, num2):
-    return num1 * num2
+    answer = num1 * num2
+    return answer
 def divide(num1, num2):
     if num2 == 0 or num1 == 0:
-        return 0
-    return num1 / num2
+        answer = 0
+        return answer
+    answer = num1 / num2
+    return answer
 def power(num1, num2):
-    return num1 ** num2
+    answer = num1 ** num2
+    return answer
--- basic_calculator/main.py
+++ basic_calculator/main.py
@@ -17,4 +17,17 @@ while True:
     elif user_input in ('1','2','3','4','5'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
-        
+        if user_input =='1':
+            print(functions.add(num1, num2))
+        elif user_input =='2':
+            print(functions.subtract(num1, num2))
+        elif user_input =='3':
+            print(functions.multiply(num1, num2))
+        elif user_input == '4':
+            print(functions.divide(num1, num2))
+        elif user_input == '5':
+            print(functions.power(num1, num2))
+        else:
+            print("invalid input")
+            
+
