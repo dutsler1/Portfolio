ff17afc3f9f9da21b56236dddd7ae3f383caf90e
dutsler1 <dutsler1@gmail.com>
2026-01-23 01:01:06 -0700

    created the function for savetaks and loadtasks

--- todo_list/tasks.py
+++ todo_list/tasks.py
@@ -1,14 +1,27 @@
+import json
+import os
+TASKS_FILE = 'tasks.json'
+
 def load_tasks():
     #this function will read tasks from file or return an empty list if file doesnt exist
-
-def save_tasks():
+    if os.path.exists(TASKS_FILE):
+        try:
+            with open(TASKS_FILE, 'r') as file:
+                return json.load(file)
+        except:
+            return []
+    else:
+        return []
+def save_tasks(tasks):
     #this function saves the task to a file
-
+    with open(TASKS_FILE, 'w') as file:
+        json.dump(tasks, file)
 def add_task():
     #this function will add a new task to the list
-
+    pass
 def complete_task():
     #this function will mark tasks as completed
-
+    pass
 def delete_task():
-    #this function will delete a task from the list
+    #this function will delete a task from the list 
+    pass
