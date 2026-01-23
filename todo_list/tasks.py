0d192755ba999970e2f551ed7a02a8fba23030d5
dutsler1 <dutsler1@gmail.com>
2026-01-23 00:39:06 -0700

    added to do list project to portfolio

---
+++ todo_list/tasks.py
@@ -0,0 +1,14 @@
+def load_tasks():
+    #this function will read tasks from file or return an empty list if file doesnt exist
+
+def save_tasks():
+    #this function saves the task to a file
+
+def add_task():
+    #this function will add a new task to the list
+
+def complete_task():
+    #this function will mark tasks as completed
+
+def delete_task():
+    #this function will delete a task from the list
