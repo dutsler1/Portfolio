03a70996b76b9648acc68a2a97882a4bdec160d2
dutsler1 <dutsler1@gmail.com>
2026-01-26 00:44:12 -0700

    completed the main.py for user input allowing user to choose to add complete delete or exit. also tested the program with no issues

---
+++ todo_list/main.py
@@ -0,0 +1,20 @@
+import tasks
+
+
+print("Welcome to your to do list")
+print("select and option:")
+print("1. Add a task\n2. Mark a task as complete\n3. Delete a task\n4. Exit menu")
+while True:
+    choice = input("Enter your choice: ")
+    if choice == '1':
+        tasks.add_task()
+    elif choice == '2':
+        tasks.complete_task()
+    elif choice == '3':
+        tasks.delete_task()
+    elif choice == '4':
+        print('Goodbye!')
+        break
+    else:
+        print("Invalid option, Try again.")
+    
