aa7f01718a0340b2a03c6aa3719f4c97a160486e
dutsler1 <dutsler1@gmail.com>
2026-01-26 03:28:47 -0700

    fixed issues with reocurring loop

--- budget_tracker/main.py
+++ budget_tracker/main.py
@@ -1,8 +1,8 @@
 import expenses
-print("Add your expenses here!")
-print("1. add expense\n2. view expenses'n3.view expense by category\n4. total spent")
-user_choice = input("Choose and option")
+
 while True:
+    print("1. add expense\n2. view expenses\n3.view expense by category\n4. total spent\n5. exit")
+    user_choice = input("Choose and option ")
     if user_choice == "1":
         expenses.add_expense()
     elif user_choice == "2":
@@ -11,6 +11,8 @@ while True:
         expenses.view_by_category()
     elif user_choice == "4":
         expenses.total_spent()
+    elif user_choice == "5":
+        break
     else:
         print("invalid choice")
 
