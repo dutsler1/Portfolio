9da9a17407e8c5aed8443be8350fe976291d3e51
dutsler1 <dutsler1@gmail.com>
2026-01-27 01:36:58 -0700

    created the functions for load_students, save_students, add_student, View_all_students and began working on sort_by_name

--- student_grade_manager/students.py
+++ student_grade_manager/students.py
@@ -1,9 +1,46 @@
 import json
 import os
 import grades
+students_file = 'students.json'
 
-#add a student()
-#view_students()
-#sort_students_by_name()
-#sort_students_by_grade()
-#remove_student()
+def load_students():
+    if os.path.exists(students_file):
+        try:
+            with open(students_file, 'r') as file:
+                return json.load(file)
+        except:
+            return []
+    else:
+        return []
+    
+def save_students(students):
+    with open(students_file, 'w') as file:
+        json.dump(students, file)
+
+def add_student():
+    students = load_students()
+    name = input("Enter student name: ")
+    student_id = students[-1]["id"] + 1 if students else 1
+    new_student = {
+        "id": student_id,
+        "name": name,
+        "grades": {}
+    }
+    students.append(new_student)
+    save_students(students)
+def view_all_students():
+    students = load_students()
+    if not students:
+        print("No students found.")
+        return
+    for student in students:
+        print(f"({student['id']}) {student['name']} - Grades: {student['grades']}")
+
+def sort_by_name():
+    students = load_students()
+    students.sort(key=lambda student: student['name'])
+    for student in students: 
+        print(f"{student['id']} - {student['name']} - Grades: {student['grades']}")
+    
+#def sort_students_by_grade()
+#def remove_student()
