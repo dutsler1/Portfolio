bc06caa0e811ec653472d2e98a7b98bad935ad86
dutsler1 <dutsler1@gmail.com>
2026-02-18 02:41:23 -0700

    added functions in grades.py to save grades and return a letter value based on the int value of the grade

--- student_grade_manager/grades.py
+++ student_grade_manager/grades.py
@@ -3,3 +3,47 @@ import os
 import students
 grades_file = 'grades.json'
 
+def load_grades():  
+    if os.path.exists(grades_file):
+        try:
+            with open(grades_file, 'r') as file:
+                return json.load(file)
+        except:
+            return {}
+    else:
+        return {}
+
+def save_grades(grades):
+    with open(grades_file, 'w') as file:
+        json.dump(grades, file)
+
+def letter_grade(score):
+    if score > 100:
+        print('Invalid score. Must be between 0 and 100.')
+        return None
+    elif score > 90 and score < 95:
+        return 'A'
+    elif score >= 95:
+        return 'A+'
+    elif score > 80 and score < 85:
+        return 'B'
+    elif score >= 85 and score <= 89:   
+        return 'B+'
+    elif score > 70 and score < 75:
+        return 'C'
+    elif score >= 75 and score <= 79:
+        return 'C+'
+    elif score > 60 and score < 65:
+        return 'D'
+    elif score >= 65 and score <= 69:
+        return 'D+'
+    elif score >= 0 and score <= 59:
+        return 'F'
+    else:        print('Invalid score. Must be between 0 and 100.')
+
+def add_grade():
+    students_list = students.load_students()
+    if not students_list:
+        print("Students must be added before adding a grade.")
+        return
+    
---
+++ student_grade_manager/main.py
@@ -0,0 +1,3 @@
+print('To add a student enter 1')
+print('To remove a student enter 2')
+print
