1feccc06b19586bac00cad13b0951f0c6b8925bb
dutsler1 <dutsler1@gmail.com>
2026-03-15 23:01:13 -0600

    added a banking app using ts and angular

---
+++ banking_app/banks/src/app/banks.ts
@@ -0,0 +1,13 @@
+import { Bank } from '.\bank';
+
+export const BANKS: Bank[] = [
+    { id: 11, name: 'Bank of America Merrill Lynch' },
+    { id: 12, name: 'Barclays Capital' },
+    { id: 13, name: 'Citi' },
+    { id: 13, name: 'Credit Suisse' },
+    { id: 14, name: 'Deutsche Bank' },
+    { id: 15, name: 'Goldman Sachs' },
+    { id: 16, name: 'J.P. Morgan' },
+    { id: 17, name: 'Morgan Stanley' },
+    { id: 18, name: 'UBS'}
+];
